adf4f55e6a351bd57a9e82636032b2d3c4f85596
Hack Reactor Students <accounts@hackreactor.com>
2015-05-02 16:53:53 -0700

    Implemented Appview and rowView ; views now display rows with titles

--- app/views/rowView.js
+++ app/views/rowView.js
@@ -1,4 +1,13 @@
 var RowView = Backbone.View.extend({
 
+  template: _.template("<div class='row'><h3><%- rowName %></h3></div>"),
+
+  initialize: function(){
+    this.render();
+  },
+
+  render: function() {
+    return this.$el.html(this.template( {rowName: this.model.get('name')} ) );
+  }
 
 });
