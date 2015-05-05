8e7b96b7f89a1cedc12d2e71fb612bc8ff802fd7
Hack Reactor Students <accounts@hackreactor.com>
2015-05-05 12:14:22 -0700

    added timer logic to control popup

--- app/views/appView.js
+++ app/views/appView.js
@@ -5,6 +5,10 @@ var AppView = Backbone.View.extend({
 
   initialize: function(){
     this.render();
+    window.watch = new this.Watch(100,1000,function(){
+      $('#popup').remove();
+      window.watch.reset();
+    });
   } ,
 
   render: function() {
@@ -16,7 +20,52 @@ var AppView = Backbone.View.extend({
 
     // Append into the DOM
     $('body').empty().append( this.$el );
+//this.watch = new this.Watch(100,5000,function(){console.log(this.currentTime);});
+  },
+  //make a timer for the popup
+  Watch: function(cycleTime, timeLimit, callback) {
+    // Define the global variable with the total time
+    this.currentTime = 0;
+    this.timeLimit =  timeLimit || 3000;
 
+    // Variable responsible by defining whether the cronometer is on or not
+    var interval;
+
+    // Start the cronometer
+    this.start = function(){
+        if (!interval){
+            interval = setInterval(this.addTime.bind(this), cycleTime || 500);
+        }
+      };
+
+    // Stop the cronometer
+    this.stop = function(){
+        if (interval){
+          clearInterval(interval);
+          interval = null;
+        }
+        return this;
+      };
+
+    // Reset the cronometer
+    this.reset = function(){
+        this.currentTime = 0;
+        this.stop();
+        return this;
+      };
+
+    this.do = function(){
+      callback();
+    };
+
+    // Increment seconds to the cronometer
+    this.addTime = function(){
+        this.currentTime += cycleTime;
+        console.log(this.currentTime);
+        if (this.currentTime >= this.timeLimit ) {
+          this.do();
+        }
+      };
   }
 });
 
