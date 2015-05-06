16cbeadcc0e45e5e324c4ddb72c331056bf9d819
Elvio Cavalcante <elvio@madfx.com.br>
2015-05-06 13:47:35 -0700

    Added the integration header file and change the name of app.js to server.js

---
+++ server/integrations/integration-headers.js
@@ -0,0 +1,4 @@
+// Headers defines which galleries the app will display.
+var headers = {
+	vimeo: ["Animation", "Arts & Design", "Cameras & Techniques", "Comedy", "Documentary", "Experimental", "Fashion", "Food", "Instructionals", "Music", "Narrative", "Personal", "Reporting & Journalism", "Sports", "Talks", "Travel"]
+}
