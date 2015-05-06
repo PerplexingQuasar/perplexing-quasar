504c7bc06758be7d249b99d0d7f3a2e6aa2d66b4
Jimmy Tjandra <jim.tjandra@gmail.com>
2015-05-06 15:55:16 -0700

    Dynamic styling based on data from server.

--- app/models/AppModel.js
+++ app/models/AppModel.js
@@ -1,6 +1,6 @@
 var AppModel = Backbone.Model.extend({
   initialize: function(){
     this.set('rowCollection', new RowCollection(data.header) );
-    console.dir(this.get('rowCollection'));
+    // console.dir(this.get('rowCollection'));
   }
 });
